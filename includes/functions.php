7affcdaf3a3183cc32aa8ceeafec730c7ba9bd83
Angel <ajin@college.harvard.edu>
2013-12-07 14:11:18 -0500

    updated format

--- includes/functions.php
+++ includes/functions.php
@@ -20,9 +20,77 @@
         }
     }
 
+    function redirect($destination)
+    {
+        // handle URL
+        if (preg_match("/^https?:\/\//", $destination))
+        {
+            header("Location: " . $destination);
+        }
+
+        // handle absolute path
+        else if (preg_match("/^\//", $destination))
+        {
+            $protocol = (isset($_SERVER["HTTPS"])) ? "https" : "http";
+            $host = $_SERVER["HTTP_HOST"];
+            header("Location: $protocol://$host$destination");
+        }
+
+        // handle relative path
+        else
+        {
+            // adapted from http://www.php.net/header
+            $protocol = (isset($_SERVER["HTTPS"])) ? "https" : "http";
+            $host = $_SERVER["HTTP_HOST"];
+            $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
+            header("Location: $protocol://$host$path/$destination");
+        }
+
+        // exit immediately since we're redirecting anyway
+        exit;
+    }
+
     function apologize($message)
     {
         render("apology.php", ["message" => $message]);
         exit;
     }
 ?>
+
+<script>
+
+    function assignment()
+    {
+        var myList=document.getElementById("myList");
+        document.getElementById("favorite").value=mylist.options[mylist.selectedIndex].text;  
+    }
+
+    /**--------------------------
+    //* Validate Date Field script- By JavaScriptKit.com
+    //* For this script and 100s more, visit http://www.javascriptkit.com
+    //* This notice must stay intact for usage
+    ---------------------------**/
+
+    function checkdate(input)
+    {   
+        //Basic check for format validity
+        var validformat=/^\d{2}\/\d{2}\/\d{4}$/ 
+        var returnval=false
+        if (!validformat.test(input.value))
+            returnval=false
+        else
+        { 
+            //Detailed check for valid date ranges
+            var monthfield=input.value.split("/")[0]
+            var dayfield=input.value.split("/")[1]
+            var yearfield=input.value.split("/")[2]
+            var dayobj = new Date(yearfield, monthfield-1, dayfield)
+            if ((dayobj.getMonth()+1!=monthfield)||(dayobj.getDate()!=dayfield)||(dayobj.getFullYear()!=yearfield))
+            returnval=false
+            else
+            returnval=true
+        }
+       return returnval;
+    }
+
+</script>
--- templates/event_form.php
+++ templates/event_form.php
@@ -1,15 +1,5 @@
 <form action="event_confirmation.php" method="post">
-<div id="assignment">
-		<script>
-		function assignment()
-		{
-			var mylist=document.getElementById("myList");
-			document.getElementById("favorite").value=mylist.options[mylist.selectedIndex].text;
-			(a.options[a.selectedIndex].value);		
-		}
-
-
-		</script>
+	<div id="type">	
 		Select your assignment:
 		<select id="myList" onchange="assignment()">
 		  <option></option>
@@ -17,49 +7,38 @@
 		  <option value="pset">pset </option>  
 		  <option value="exam">exam </option>
 		</select>
-
 		<p>Your assignment is: a <input autofocus class="form-control" name="type" placeholder="type of assignment" type="text" id="favorite" size="20"></p>
+		<br>
+	</div>
 
-		<!-- form for paper
-		<div id="paperform" Object.style.visibility="visible"><p>
-		hours needed for: <br> 
-			1) research: <input type="text" id="research" size="20"><br>
-			2) outlining: <input type="text" id="outlining" size="20"><br></p>
-		</div>
-		ii 
-		-->
-	<br>
-</div>
+	<div id="name">
+		<?php 
+			echo "Create name for assignment.";
+		?>
+		<input autofocus class="form-control" name="name" placeholder="name of assignment" type="text"/>  	
+		<br>
+		<br>
+	</div>
 
-<div id="name">
-	<?php 
-		echo "Create name for assignment.";
-	?>
-	<input autofocus class="form-control" name="name" placeholder="name of assignment" type="text"/>
-	  	
-	<br>
-	<br>
-</div>
-
-<div id="due date">
-	<?php 
-		echo "Select due date.";
-	?>
-
-	<br>
-	<script type="text/javascript" src="http://www.snaphost.com/jquery/Calendar.aspx"></script> 
-	<br>
-	<br>
-</div>
-<div id="hours spent">
-	<?php 
-		echo "Input number of hours to be spent on this assignment.";
-	?>
-	<br>
-	<input autofocus class="form-control" name="hours" placeholder="hours spent" type="text"/>
-	  	
-	<br>
-	<br>
-</div>
-<button type="submit">Submit</button>
+	<div id="date">
+		<?php 
+			echo "Select due date.";
+		?>
+		<br>
+		<input autofocus class="form-control" name="date" placeholder="due date" type="text"/>
+		<b>Valid date format:</b> mm/dd/yyyy<br />
+		<br>
+		<br>
+	</div>
+	<div id="hours">
+		<?php 
+			echo "Input number of hours to be spent on this assignment.";
+		?>
+		<br>
+		<input autofocus class="form-control" name="hours" placeholder="hours spent" type="text"/>	
+		<br>
+		<br>
+	</div>
+	<button type="submit" name="submit">Submit</button>
 </form> 
+
